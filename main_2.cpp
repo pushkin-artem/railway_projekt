5ab0733a0b88bd5df739916b6163de0a5a290c4a
pushkin-artem <57641801+pushkin-artem@users.noreply.github.com>
2020-01-16 20:23:08 +0100

    Add files via upload

--- main_2.cpp
+++ main_2.cpp
@@ -1,27 +1,18 @@
 /*
-
 Menetrend: Egy A-B-C útvonal:
-
 V1 2 //vonat nev es hogy hany kocsit bir el  // название поезда, и сколько вагонов можно прицеплять
 A 1
 B 2
 C 4
 B 6
 A 7
-
 Kocsik:
-
 a 10 A   //kocsi nev, kapacitasa, es hol van  // название вагона, его вместимость и его место нахождение
 b 15 B
-
 Áruk:
-
 cucc1 A C 35  //csomag nev, indulo es celhelye, hany db
 cucc2 B C 15
-
-
 Kimeneti akcióterv:
-
 0 pakol a cucc1 10
 // van egy óránk pakolni, mert csak 1-kor indul a vonat
 0 pakol b cucc2 15
@@ -45,23 +36,18 @@ Kimeneti akcióterv:
 18 lecsatol b V1
 19 kipakol a cucc1 10
 19 kipakol b cucc1 15
-
-
 Adott órában kocsi töltése áruval (csak nem csatlakoztatott kocsit szabad tölteni)
 Adott órában kocsi csatlakoztatása vonathoz (nem lehet ugyanazon órában tölteni és csatlakoztatni)
 Adott órában kocsi leválasztása kocsiról (egy órában egy kocsi leválasztása és csatlakoztatása átcsatolást jelent egy másik vonatra, szabad lecsatolni és pakolni ugyanazon órában)
 Adott órában kocsi kipakolása (csak nem csatlakoztatott kocsit szabad kipakolni)
-
 Nem szabad 1 oraban:
             - lecsatolni es kipakolni
             - csatolni es pakolni
-
 Tehat 1 ora alatt vegzunk a kovetkezoket:
             - kocsi csatolas (csak nem csatolt kocsit szabad pakolni)
             - kocsi lecsatolasa
             - kocsi pakolas (lehet lecsatolni es pakolni egy oraban)
             - kocsi kipakolas (csak nem csatolt kocsit szabad kipakolni)
-
             - egy órában egy kocsi lecsatolasa és csatolasa átcsatolást jelent egy másik vonatra
 */
 
@@ -76,7 +62,7 @@ using namespace std;
 
 struct Vonat
 {
-    string vonat_nev;
+    string vonat_nev="vvv";
     int vonat_biras = 0;
 };
 
@@ -84,13 +70,13 @@ struct Kocsi
 {
     string kocsi_nev;
     int kocsi_kapacitas = 0;
-    string kocsi_helye;
+    string kocsi_helye ="x";
 };
 
 struct Csomag
 {
     string csomag_nev;
-    string csomag_kezdohelye, csomag_celhelye;
+    string csomag_kezdohelye = "x", csomag_celhelye="x";
     int csomag_mennyiseg = 0;
 };
 
@@ -98,17 +84,16 @@ class Allomas
 {
 public:
 
-    //Vonat vonat;
-    string allomas_nev;
+    Vonat vonat;
+    string allomas_nev="";
     int allomas_ora;
+    string allomas_vonat = "";
     Kocsi kocsi;
     Csomag csomag;
-
-    //Allomas(Vonat vonat1){vonat = vonat1;}
 };
 
 
-void beolvasas(string fajlnev, vector<Allomas>& allomasok)
+void beolvasas(string fajlnev, Allomas& a, vector<Allomas>& allomasok)
 {
     ifstream befajl(fajlnev);
 
@@ -118,19 +103,16 @@ void beolvasas(string fajlnev, vector<Allomas>& allomasok)
         return;
     }
 
-    Vonat v;
-
-    Allomas a;
-
+    //Allomas a;
     string tmp;
 
     if(fajlnev == "vonat.txt")
     {
         while(befajl.good())
         {
-            getline(befajl, v.vonat_nev, ' ');
+            getline(befajl, a.vonat.vonat_nev, ' ');
             getline(befajl, tmp);
-            v.vonat_biras = atoi(tmp.c_str());
+            a.vonat.vonat_biras = atoi(tmp.c_str());
         }
         //cout << v.vonat_nev << " " << v.vonat_biras << endl;
     }
@@ -195,8 +177,7 @@ void beolvasas(string fajlnev, vector<Allomas>& allomasok)
 
 void szabalyok(vector<Allomas> allomasok)
 {
-
-    for(x : allomasok)
+    for(auto x : allomasok)
     {
         cout << "Allomas nev: " << x.allomas_nev << endl;
         cout << "Ora: " << x.allomas_ora << endl;
@@ -207,28 +188,67 @@ void szabalyok(vector<Allomas> allomasok)
     cout << endl;
 
     int ido = 0;
+    bool direction = 1;
 
-    for(x : allomasok)
-    {
-        if(x.kocsi.kocsi_helye != "" && x.csomag.csomag_kezdohelye != "")
+    while(true)
+    {    //заменить рекурсией или другим условием
+        cout<< "Time is: "<< ido << endl;
+    //Первый шаг - проверка всех станций в момент времени 0
+        for(auto x : allomasok)
         {
-            if(x.kocsi.kocsi_helye == x.csomag.csomag_kezdohelye)
-            {
-                if(x.csomag.csomag_mennyiseg >= x.kocsi.kocsi_kapacitas)
-                {
-                    x.csomag.csomag_mennyiseg =  x.csomag.csomag_mennyiseg - x.kocsi.kocsi_kapacitas;
-                    cout << ido << " pakol " << x.kocsi.kocsi_nev << " " << x.csomag.csomag_nev << " " << x.kocsi.kocsi_kapacitas << endl;
-                    x.kocsi.kocsi_kapacitas  = 0;
 
-                }
-                else
+    //Поезд
+            /*
+            if( x.allomas_nev == allomasok[ido].allomas_nev)  //связать V1 и расписание
+            {
+                cout << "Menetrend: " << x.allomas_nev << " " << x.allomas_ora << ", Vonat:  " << x.vonat.vonat_nev << " " << x.vonat.vonat_biras << endl;
+            }
+            */
+            if(ido == x.allomas_ora)
+            {
+                cout << "Menetrend: " << x.allomas_nev << " " << x.allomas_ora << ", Vonat:  " << x.vonat.vonat_nev << " " << x.vonat.vonat_biras << endl;
+            }
+
+    //Грузы и вагоны на станциях
+            if(x.kocsi.kocsi_helye != "x" && x.csomag.csomag_kezdohelye != "x")
+            {
+                if(x.kocsi.kocsi_helye == x.csomag.csomag_kezdohelye)
                 {
-                    x.kocsi.kocsi_kapacitas =  x.kocsi.kocsi_kapacitas - x.csomag.csomag_mennyiseg;
-                    cout << ido << " pakol " << x.kocsi.kocsi_nev << " " << x.csomag.csomag_nev << " " << x.csomag.csomag_mennyiseg << endl;
-                    x.csomag.csomag_mennyiseg  = 0;
+                    if(x.csomag.csomag_mennyiseg >= x.kocsi.kocsi_kapacitas)
+                    {
+                        x.csomag.csomag_mennyiseg =  x.csomag.csomag_mennyiseg - x.kocsi.kocsi_kapacitas;
+                        cout << ido << " pakol " << x.kocsi.kocsi_nev << " " << x.csomag.csomag_nev << " " << x.kocsi.kocsi_kapacitas << endl;
+                        x.kocsi.kocsi_kapacitas  = 0;
+                    }
+                    else
+                    {
+                        x.kocsi.kocsi_kapacitas =  x.kocsi.kocsi_kapacitas - x.csomag.csomag_mennyiseg;
+                        cout << ido << " pakol " << x.kocsi.kocsi_nev << " " << x.csomag.csomag_nev << " " << x.csomag.csomag_mennyiseg << endl;
+                        x.csomag.csomag_mennyiseg  = 0;
+                    }
                 }
             }
         }
+
+        if (ido == allomasok[allomasok.size()-1].allomas_ora)
+        {
+            //cout<<"The end of the vector is reached"<<endl;
+            direction=0;
+        }
+
+        if (ido == allomasok[0].allomas_ora)
+        {
+            cout<<"The begining of the vector is reached"<<endl;
+            direction=1;
+        }
+
+        if(direction==1)
+            ido=ido+1;
+        else
+            ido=ido-1;
+
+            system("pause");
+            cout << endl;
     }
 
     /*
@@ -245,20 +265,15 @@ void szabalyok(vector<Allomas> allomasok)
 
 int main()
 {
+    Allomas a;
     vector<Allomas> allomasok;
 
-    beolvasas("vonat.txt", allomasok);
-    beolvasas("menetrend.txt", allomasok);
-    beolvasas("kocsik.txt", allomasok);
-    beolvasas("csomagok.txt", allomasok);
+    beolvasas("vonat.txt", a, allomasok);
+    beolvasas("menetrend.txt", a, allomasok);
+    beolvasas("kocsik.txt", a, allomasok);
+    beolvasas("csomagok.txt", a, allomasok);
 
     szabalyok(allomasok);
-
-    //cout << v.vonat_nev << endl;
-
-    //Allomas x(v, m);
-    //cout << x.vonat.vonat_nev << " " << x.vonat.vonat_biras << endl;
-    //cout << x.menetrend.allomas_nev << endl;
 
     return 0;
 }
